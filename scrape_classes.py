53b9eb88ee24e9c04c9f482dc1113d5badbc7efa
johannesbyle <johannesbyle1@gmail.com>
2021-01-28 12:11:55 +0100

    now scrapes all data, but very slowly

--- scrape_classes.py
+++ scrape_classes.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 import os
 from selenium.webdriver.support.select import Select
 import pandas as pd
-import numpy as np
+from tqdm import tqdm
 
 # Initialize driver
 driver = webdriver.Chrome()
@@ -22,17 +22,15 @@ majors = Select(driver.find_element_by_xpath('//*[@id="subj_id"]'))
 [majors.select_by_index(n) for n in range(len(majors.options))]
 driver.find_element_by_xpath("/html/body/div[3]/form/input[17]").click()
 
-# Scrape the course data
-df = pd.DataFrame(columns=["major", "Num", "Name"])
-majors = driver.find_elements_by_class_name("datadisplaytable")
-for major in majors:
-    major_name = major.find_elements_by_class_name("ddheader")
-    if not major_name:
-        continue
-    major_name = major_name[0].text
-    courses = major.find_elements_by_class_name("dddefault")
-    courses = np.array([n.text for n in courses])
-    for course in courses.reshape(-1, 2):
-        df = df.append({"major": major_name, "Num": course[0], "Name": course[1]}, ignore_index=True)
-
-df.to_feather("courses.feather")
+# Scrape the course data by clicking through each class
+dfs = []
+for n in tqdm(range(len(driver.find_elements_by_css_selector("input[value='View Sections']")))):
+    inputs = driver.find_elements_by_css_selector("input[value='View Sections']")
+    inputs[n].click()
+    table = pd.read_html(driver.find_element_by_xpath("//body").get_attribute('outerHTML'))[5]
+    major = table.columns.get_level_values(0)[0]
+    if [n for n in table.columns.get_level_values(0) if n != major]:
+        raise Exception("Not just one major")
+    dfs.append(table[major])
+    driver.back()
+pd.concat(dfs).reset_index(drop=True).to_feather("courses.feather")

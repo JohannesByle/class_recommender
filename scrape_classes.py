c4664fb40fa6ad0a5ee2b62b4a9375a75b708e3a
JohannesByle <johannesbyle1@gmail.com>
2021-02-03 17:46:20 +0100

    now scrapes all courses all the way back from 2003

--- scrape_classes.py
+++ scrape_classes.py
@@ -6,24 +6,30 @@ from tqdm import tqdm
 import json
 
 os.chdir(os.path.dirname(__file__))
-if not os.path.exists("data"):
-    os.mkdir("data")
+dirs = ["data"]
+[os.mkdir(n) for n in dirs if not os.path.exists(n)]
+
 requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS += ':HIGH:!DH:!aNULL'
 auth_data = {"sid": os.environ["PORTAL_USER_ID"], "PIN": os.environ["PORTAL_PASSWORD"]}
 
 
 def clean_df(df):
     df = df.dropna(subset=["CRN"]).drop_duplicates().reset_index(drop=True)
+    df = df[df["Associated Term"] != "Associated Term"]
+    df = df[[not all([df.iloc[n, 0] == df.iloc[n, i] for i in range(len(df.iloc[n]))]) for n in range(len(df.index))]]
     df["CRN"] = df["CRN"].astype(int)
     return df
 
 
 def course_to_pandas(html):
     table = pd.read_html(html)[5]
-    major = table.columns.get_level_values(0)[0]
-    if [n for n in table.columns.get_level_values(0) if n != major]:
+    year = table.columns.get_level_values(0)[0]
+    if [n for n in table.columns.get_level_values(0) if n != year]:
+        raise Exception("Not just one year")
+    major = table[year].columns.get_level_values(0)[0]
+    if [n for n in table[year].columns.get_level_values(0) if n != major]:
         raise Exception("Not just one major")
-    return table[major]
+    return table[year][major]
 
 
 def form_to_json(html, action):
@@ -56,22 +62,30 @@ def get_course(subject, course, year):
         return course_to_pandas(p.text)
 
 
-def get_all_courses(year):
+def get_all_courses():
     dfs = []
     with requests.session() as s:
         s.get("https://bannerweb.wheaton.edu/db1/bwckgens.p_proc_term_date")
         s.post("https://bannerweb.wheaton.edu/db1/twbkwbis.P_ValLogin", data=auth_data)
         p = s.get("https://bannerweb.wheaton.edu/db1/bwskfcls.p_sel_crse_search")
         form = form_to_json(p.text, "/db1/bwckgens.p_proc_term_date")[0]
-        form = [n for n in form if n[0] != "p_term" or n[1] == year]
-        p = s.post("https://bannerweb.wheaton.edu/db1/bwckgens.p_proc_term_date", data=form)
-        form = form_to_json(p.text, "/db1/bwskfcls.P_GetCrse")[0]
-        form.append(("SEL_CRSE", "dummy"))
-        form.append(("SEL_TITLE", "dummy"))
-        p = s.post("https://bannerweb.wheaton.edu/db1/bwskfcls.P_GetCrse", data=form)
-        forms = form_to_json(p.text, "/db1/bwskfcls.P_GetCrse")
-        for form in tqdm(forms):
+        years = [n for n in form if n[0] == "p_term" and n[1] and not n[1].endswith("05")]
+        form_master = [n for n in form if n[0] != "p_term"]
+        years_form = [("term_in", n[1]) for n in years]
+        courses = []
+        for year in years[:4]:
+            form = form_master + [year]
+            p = s.post("https://bannerweb.wheaton.edu/db1/bwckgens.p_proc_term_date", data=form)
+            form = form_to_json(p.text, "/db1/bwskfcls.P_GetCrse")[0]
+            form.append(("SEL_CRSE", "dummy"))
+            form.append(("SEL_TITLE", "dummy"))
             p = s.post("https://bannerweb.wheaton.edu/db1/bwskfcls.P_GetCrse", data=form)
-            dfs.append(course_to_pandas(p.text))
-
-    clean_df(pd.concat(dfs)).to_pickle("data/courses.p")
+            forms = form_to_json(p.text, "/db1/bwskfcls.P_GetCrse")
+            for form in tqdm(forms):
+                subj = [n[1] for n in form if n[0] == "sel_subj" and n[1] != "dummy"][0]
+                course = [n[1] for n in form if n[0] == "SEL_CRSE"][0]
+                if not (subj, course) in courses:
+                    courses.append((subj, course))
+                    p = s.post("https://bannerweb.wheaton.edu/db1/bwskfcls.P_GetCrse", data=form + years_form)
+                    dfs.append(course_to_pandas(p.text))
+        clean_df(pd.concat(dfs)).to_pickle("data/courses.p")

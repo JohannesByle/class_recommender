78a56ba9a78f7add321d887288c9e767060e75e2
JohannesByle <johannesbyle1@gmail.com>
2021-02-11 09:41:45 -0600

    now parses xml directly

--- parse_requirements.py
+++ parse_requirements.py
@@ -1,6 +1,8 @@
 from xml.etree import ElementTree
 import os
 import json
+from warnings import warn
+import pandas as pd
 
 os.chdir(os.path.dirname(__file__))
 if not os.path.exists("data"):
@@ -9,83 +11,97 @@ if not os.path.exists("data/requirements_json"):
     os.mkdir("data/requirements_json")
 
 
-def print_recursive(node, tab=0):
+def print_recursive(array, tab=0):
     indent = "".join(["\t"] * tab)
-    if isinstance(node, dict) and "Label" in node:
-        how_many = "Any"
-        if node["Type"] == "Subset":
-            how_many = "All"
-        elif node["Type"] == "Group":
-            how_many = "Choose {}".format(node["Num Required"])
-        elif node["Type"] == "Course" and node["Min Credits"]:
-            how_many = "At least {} credits".format(node["Min Credits"])
-        print("{}{} - {}".format(indent, node["Label"], how_many))
-        [print_recursive(n, tab + 1) for n in node["Rule"]]
-    elif isinstance(node, dict) and "Course" in node:
-        print(indent + str(list(node["Course"].values())))
-    elif isinstance(node, list):
-        [print_recursive(n, tab + 1) for n in node]
+    if isinstance(array, dict):
+        print(indent + str(list(array.keys())[0]))
+        print_recursive(list(array.values())[0], tab=tab + 1)
+    elif isinstance(array, list):
+        for item in array:
+            print_recursive(item, tab=tab + 1)
+    elif isinstance(array, tuple):
+        print(indent + str(array[0]))
+        print_recursive(array[0], tab=tab + 1)
+    elif isinstance(array, str) or isinstance(array, bool):
+        print(indent + str(array))
 
 
-def parse_rule(rule):
-    children = [n.tag for n in rule]
-    label = rule.attrib["Label"]
-    rule_type = rule.attrib["RuleType"]
-    if rule_type == "IfStmt":
-        is_else = "IfElsePart" in children and rule[children.index("IfElsePart")].text == "ElsePart"
-        boolean = rule[children.index("BooleanEvaluation")].text == "True"
-        requirement_children = rule.find("Requirement")
-        requirement_children_tags = [n.tag for n in requirement_children]
-        name = "ElsePart" if (is_else or not boolean) and "ElsePart" in requirement_children_tags else "IfPart"
-        return parse_xml(requirement_children[requirement_children_tags.index(name)])
-    elif rule_type == "Course":
-        if len(rule.findall("Requirement")) > 1:
-            raise Exception("More than one requirement")
-        min_credits, courses = parse_req(rule.find("Requirement"))
-        return {"Label": label, "Rule": courses, "Type": rule_type, "Min Credits": min_credits}
-    elif rule_type == "Group":
-        return {"Label": label, "Num Required": rule.find("Requirement").attrib["NumGroups"],
-                "Rule": parse_xml(rule), "Type": rule_type}
+def switch(functions, option, ignored=None):
+    if option in functions:
+        return functions[option]
     else:
-        return {"Label": label, "Rule": parse_xml(rule), "Type": rule_type}
-
-
-def parse_req(req):
-    def parse_course(course):
-        course_data = {"Course": {"Name": course.attrib["Disc"], "Number": course.attrib["Num"]}}
-        children_tags = [n.tag for n in course]
-        if "With" in children_tags:
-            with_node = course[children_tags.index("With")]
-            course_data["Course"][with_node.attrib["Code"]] = [n.text for n in with_node if n.tag == "Value"][0]
-        return course_data
-
-    if req.attrib["Class_cred_op"] != "OR":
-        raise Exception("Not or exception")
-    min_cred = None
-    if "Credits_begin" in req.attrib:
-        min_cred = req.attrib["Credits_begin"]
-    return min_cred, [parse_course(n) for n in req.findall("Course")]
-
-
-def parse_xml(node):
-    if node.tag == "Block" and node.attrib["Req_type"] == "DEGREE":
+        if not (ignored and option in ignored):
+            # warn("'{}' is not a known option".format(option), stacklevel=3)
+            return
         return
-    rules = [parse_rule(n) if n.tag == "Rule" else parse_xml(n) for n in node]
-    to_return_rules = []
-    for rule in rules:
-        if not rule:
-            continue
-        if isinstance(rule, list) and len(rule) == 1:
-            to_return_rules.append(rule[0])
-            continue
-        to_return_rules.append(rule)
-    if len(to_return_rules) == 1:
-        to_return_rules = to_return_rules[0]
-    return to_return_rules
 
 
-def convert_xml():
-    reqs_dir = "data/requirements_xml"
-    for file in os.listdir(reqs_dir):
-        with open("data/requirements_json/" + file.replace(".xml", ".json"), "w") as f:
-            json.dump(parse_xml(ElementTree.parse(reqs_dir + "/" + file).getroot()), f)
+def remove_nests(array):
+    if isinstance(array, list):
+        array = [n for n in array if not (isinstance(n, list) and not n)]
+        if len(array) == 1:
+            array = array[0]
+    return array
+
+
+def rule_if_statement(courses, node):
+    if_else = "ElsePart" if node.find("BooleanEvaluation").text == "True" else "IfPart"
+    return requirements_satisfied(courses, node.find("Requirement").find(if_else))
+
+
+def rule_group(courses, node):
+    num_groups = int(node.find("Requirement").attrib["NumGroups"])
+    reqs_satisfied = [parse_rule(courses, n) for n in node.findall("Rule")]
+    num_satisfied = len([n for n in reqs_satisfied if (n[0] if isinstance(n, tuple) else all(n.values()))])
+    return num_satisfied >= num_groups, remove_nests(reqs_satisfied)
+
+
+def rule_subset(courses, node):
+    assert all([n.tag == "Qualifier" for n in node.find("Requirement")])
+    assert all([n.tag in ["IfElsePart", "Remark", "Requirement", "Rule"] for n in node])
+    return remove_nests([parse_rule(courses, n) for n in node.findall("Rule")])
+
+
+def parse_rule(courses, node):
+    fun = switch({"IfStmt": rule_if_statement, "Group": rule_group, "Subset": rule_subset}, node.attrib["RuleType"])
+    to_return = fun(courses, node) if fun else [requirements_satisfied(courses, n) for n in node]
+    if node.attrib["RuleType"] == "IfStmt":
+        return remove_nests(to_return)
+    else:
+        return {node.attrib["Label"]: remove_nests(to_return)}
+
+
+def parse_requirement(courses, node):
+    tracked_attributes = ["Classes_begin", "Class_cred_op", "Connector", "Credits_end", "Credits_begin"]
+    assert all([n.tag in ["Course", "Qualifier"] for n in node])
+    assert all([n.attrib["Name"] == "LOWESTPRIORITY" for n in node.findall("Qualifier")])
+    assert all([n in tracked_attributes for n in node.attrib])
+    sat_courses_all = pd.DataFrame()
+    for course in node.findall("Course"):
+        sat_courses = courses.copy()
+        assert all([n.tag in ["With"] for n in course])
+        for attrib in ["Disc", "Num"]:
+            sat_courses[attrib] = sat_courses[attrib].astype(str)
+            sat_courses = sat_courses[sat_courses[attrib].str.match(course.attrib[attrib].replace("@", ".+"))]
+        for with_node in course.findall("With"):
+            assert all([n in ["Code", "Operator", "Connector"] for n in with_node.attrib])
+            assert with_node.attrib["Code"] in ["DWTERM", "ATTRIBUTE"]
+            if with_node.attrib["Code"] == "ATTRIBUTE":
+                tags = [n.text for n in with_node.findall("Value")]
+                sat_courses = sat_courses[sat_courses["ATTRIBUTE"].apply(lambda x: all([n in x for n in tags]))]
+        sat_courses_all = pd.concat([sat_courses_all, sat_courses])
+    satisfied = not sat_courses_all.empty
+    if "Classes_begin" in node.attrib and satisfied:
+        satisfied = len(sat_courses_all.index) >= int(node.attrib["Classes_begin"])
+    if "Credits_begin" in node.attrib and satisfied:
+        satisfied = pd.to_numeric(sat_courses_all["Credits"]).sum() >= int(node.attrib["Credits_begin"])
+    return satisfied
+
+
+def requirements_satisfied(courses, node):
+    if node.tag == "Block" and node.attrib["Req_type"] == "DEGREE":
+        return []
+    ignored = ["Classes_applied", "Credits_applied", "ClassesApplied"]
+    fun = switch({"Rule": parse_rule, "Requirement": parse_requirement}, node.tag, ignored=ignored)
+    to_return = fun(courses, node) if fun else [requirements_satisfied(courses, n) for n in node]
+    return remove_nests(to_return)

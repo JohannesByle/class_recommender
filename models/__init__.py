4acde173f0bdc154f07b72af64421c58270cf5bc
JohannesByle <johannesbyle1@gmail.com>
2021-03-01 12:03:52 -0600

    attributes parsing now part of models

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,10 @@
 from flask_login import UserMixin
 from flask_app import db
+import json
+import os
+
+with open(os.path.join(os.path.dirname(__file__), "tags.json"), "r") as f:
+    tags = json.load(f)
 
 
 class User(UserMixin, db.Model):
@@ -9,3 +14,34 @@ class User(UserMixin, db.Model):
     email_verified = db.Column(db.Boolean())
     email_code = db.Column(db.String(64))
     password_reset_code = db.Column(db.String(64))
+
+
+class Class(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    term = db.Column(db.String(15))
+    crn = db.Column(db.Integer)
+    subj = db.Column(db.String(4))
+    crse = db.Column(db.String(5))
+    sec = db.Column(db.String(3))
+    cmp = db.Column(db.String(2))
+    cred = db.Column(db.String(15))
+    title = db.Column(db.String(100))
+    days = db.Column(db.String(7))
+    time = db.Column(db.String(25))
+    cap = db.Column(db.Integer)
+    act = db.Column(db.Integer)
+    rem = db.Column(db.Integer)
+    wl_cap = db.Column(db.Integer)
+    wl_act = db.Column(db.Integer)
+    wl_rem = db.Column(db.Integer)
+    xl_cap = db.Column(db.Integer)
+    xl_act = db.Column(db.Integer)
+    xl_rem = db.Column(db.Integer)
+    instructor = db.Column(db.String(500))
+    date = db.Column(db.String(15))
+    location = db.Column(db.String(25))
+    attribute = db.Column(db.String(200))
+
+
+def extract_attributes(attribute_string):
+    return [tags[n] for n in attribute_string.split(" and ") if n in tags]

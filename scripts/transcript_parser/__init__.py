370a9f65de04e87b8545b30558ce086ec3e02035
JohannesByle <johannesbyle1@gmail.com>
2021-03-01 12:03:47 -0600

    attributes parsing now part of models

--- scripts/transcript_parser/__init__.py
+++ scripts/transcript_parser/__init__.py
@@ -4,17 +4,8 @@ import pandas as pd
 import re
 import json
 
-courses_df = pd.read_pickle(os.path.join(os.path.dirname(os.path.dirname(__file__)), "class_scraper/data/courses.p"))
-with open(os.path.join(os.path.dirname(__file__), "tags.json"), "r") as f:
-    tags = json.load(f)
 
-
-def get_extra_course_data(disc, num):
-    df = courses_df.copy()
-    df = df[(df["Subj"] == disc) & (df["Crse"].astype(str) == num)]
-    df["Attribute"] = df["Attribute"].astype(str).apply(lambda x: [tags[n] for n in x.split(" and ") if n in tags])
-    attributes = df["Attribute"].iloc[0]
-    return {"ATTRIBUTE": attributes}
+# courses_df = pd.read_pickle(os.path.join(os.path.dirname(os.path.dirname(__file__)), "class_scraper/data/courses.p"))
 
 
 def parse_transcript(html_str):
@@ -30,7 +21,7 @@ def parse_transcript(html_str):
             name = cells[0]
             grade, cred = [None, cells[1]] if re.match(r"\d{1,2}\.\d{3}", cells[1].strip()) else cells[1:3]
             new_course = {"Disc": subj, "Num": number, "name": name, "grade": grade, "Credits": cred}
-            new_course.update(get_extra_course_data(subj, number))
+            # new_course.update(get_extra_course_data(subj, number))
             courses.append(new_course)
 
     courses = pd.DataFrame(courses)

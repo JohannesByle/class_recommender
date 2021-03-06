f92213971edd8a98ff73820901e28b4dc45d4807
sharondunbar <sharon.dunbar@my.wheaton.edu>
2021-03-06 17:27:02 -0600

    added upload button correctly

--- flask_app/views/upload_transcript.py
+++ flask_app/views/upload_transcript.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, request
 
 upload_transcript_blueprint = Blueprint("upload_transcript", __name__)
 
@@ -6,3 +6,9 @@ upload_transcript_blueprint = Blueprint("upload_transcript", __name__)
 @upload_transcript_blueprint.route('/upload_transcript')
 def upload_transcript():
     return render_template("upload_transcript/upload_transcript.html")
+
+
+@upload_transcript_blueprint.route('/upload_transcript', methods=['POST'])
+def upload_transcript_post():
+    transcript = request.form.get('userFile')
+    return redirect("/sign_up")

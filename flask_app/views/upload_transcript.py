155b9e86e9b57aa88090524a03fde2eb28f04f9f
sharondunbar <sharon.dunbar@my.wheaton.edu>
2021-03-06 20:45:45 -0600

    transcript saved to current directory for testing

--- flask_app/views/upload_transcript.py
+++ flask_app/views/upload_transcript.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, redirect, request
+from scripts.transcript_parser import parse_transcript
 
 upload_transcript_blueprint = Blueprint("upload_transcript", __name__)
 
@@ -10,5 +11,17 @@ def upload_transcript():
 
 @upload_transcript_blueprint.route('/upload_transcript', methods=['POST'])
 def upload_transcript_post():
-    transcript = request.form.get('userFile')
+    # transcript = request.form.get('userFile')
+    # print(transcript)
+    # url = r'C:\Users\Sharon\Downloads\AcademicTranscript.html'
+    # page = open(url)
+    # courses = parse_transcript(page)
+    # courses.to_html()
+
+    uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+        uploaded_file.save(uploaded_file.filename)
+
+
     return redirect("/sign_up")
+

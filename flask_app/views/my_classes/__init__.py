bcb93a0e44d8bd7e39c0ae449e1ad88b151c8941
JohannesByle <johannesbyle1@gmail.com>
2021-03-16 17:17:06 -0500

    now login required

--- flask_app/views/my_classes/__init__.py
+++ flask_app/views/my_classes/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, redirect, request, abort
 import os
 from .parse_transcript import parse_transcript
-import json
+from flask_login import login_required
 
 path = os.path.dirname(__file__)
 js_path = os.path.join(os.path.dirname(os.path.dirname(path)), "templates/my_classes/react_scripts/dist/main.js")
@@ -9,6 +9,7 @@ my_classes_blueprint = Blueprint("my_classes", __name__)
 
 
 @my_classes_blueprint.route("/my_classes")
+@login_required
 def my_classes():
     with open(js_path, "r", encoding="utf8") as f:
         js = f.read()
@@ -16,6 +17,7 @@ def my_classes():
 
 
 @my_classes_blueprint.route("/upload_transcript", methods=['POST'])
+@login_required
 def upload_transcript():
     courses = parse_transcript(request.data.decode("utf-8"))
     return courses.to_json(orient="records")

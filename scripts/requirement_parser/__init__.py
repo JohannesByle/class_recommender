7154270492c2b93493941e51c80c14d89e20fbbf
JohannesByle <johannesbyle1@gmail.com>
2021-03-23 00:32:23 -0500

    all reqs work except multi-options and "Writing"

--- scripts/requirement_parser/__init__.py
+++ scripts/requirement_parser/__init__.py
@@ -1,6 +1,36 @@
 from warnings import warn
 from xml.etree import ElementTree
 import numpy as np
+import itertools
+import os
+import pandas as pd
+import json
+from scripts.class_functions import extract_attributes, get_min_max
+
+path = os.path.dirname(__file__)
+with open(os.path.join(path, "../class_scraper/class_conversion.json"), "r") as f:
+    conversion_dict = json.load(f)
+column_conversion = {"attribute": "ATTRIBUTE", "crse": "Num", "subj": "Disc", "cred": "Credits"}
+
+
+def convert_df_to_degree_works(input_df):
+    output_df = input_df.copy()
+    output_df = output_df.rename(axis=1, mapper={conversion_dict[n]: n for n in conversion_dict})
+    output_df = output_df.rename(axis=1, mapper=column_conversion)
+    output_df["ATTRIBUTE"] = output_df["ATTRIBUTE"].apply(lambda x: extract_attributes(str(x)) if x else x)
+    return output_df
+
+
+def clean_df(input_df):
+    output_df = input_df.copy()
+    output_df = convert_df_to_degree_works(output_df)
+    output_df["Credits"] = output_df["Credits"].apply(lambda x: get_min_max(str(x))[0] if x else x)
+    years = output_df["term"].apply(lambda x: int(x[-2:] if "Term" not in x else x[-7:-5]))
+    output_df = output_df[years >= years.max() - 4]
+    return output_df
+
+
+courses_df = clean_df(pd.read_pickle(os.path.join(path, "../class_scraper/data/courses.p")))
 
 
 def flatten_reqs(array, parent=None, depth=0):
@@ -59,25 +89,23 @@ def switch(functions, option, ignored=None, silent=True):
         return
 
 
-def remove_nests(array):
-    if isinstance(array, list):
-        array = [n for n in array if not (isinstance(n, list) and not n)]
-        if len(array) == 1:
-            array = array[0]
-    return array
+def flatten(array):
+    if not isinstance(array, list) or not all([isinstance(n, list) for n in array]):
+        return array
+    return [n for n in list(itertools.chain.from_iterable(array)) if n]
 
 
-def parse_xml(courses, node):
+def parse_xml(node):
     from .parse_rule import parse_rule
     from .parse_requirement import parse_requirement
     if node.tag == "Block" and node.attrib["Req_type"] == "DEGREE":
         return []
     ignored = ["Classes_applied", "Credits_applied", "ClassesApplied"]
     fun = switch({"Rule": parse_rule, "Requirement": parse_requirement}, node.tag, ignored=ignored)
-    to_return = fun(courses, node) if fun else [parse_xml(courses, n) for n in node]
-    return remove_nests(to_return)
+    to_return = fun(node) if fun else [parse_xml(n) for n in node]
+    return flatten(to_return)
 
 
-def requirements_satisfied(courses, filename):
+def requirements_satisfied(filename):
     xml = ElementTree.parse(filename)
-    return parse_xml(courses, xml.getroot())
+    return parse_xml(xml.getroot())
--- scripts/requirement_parser/parse_requirement.py
+++ scripts/requirement_parser/parse_requirement.py
@@ -1,50 +1,67 @@
 import pandas as pd
-from . import switch
+from . import switch, courses_df
 
 
-def parse_with(courses, node):
+def parse_with(courses_input, node):
+    courses = courses_input.copy()
     assert all([n in ["Code", "Operator", "Connector", "HideFromAdvice"] for n in node.attrib])
     assert node.attrib["Code"] in ["ATTRIBUTE"] or node.attrib["Code"].startswith("DW")
-    sat_courses = courses.copy()
     if node.attrib["Code"] == "ATTRIBUTE":
         tags = [n.text for n in node.findall("Value")]
-        sat_courses = sat_courses[sat_courses["ATTRIBUTE"].apply(lambda x: all([n in x for n in tags]))]
-    return sat_courses
+        courses = courses[courses["ATTRIBUTE"].apply(lambda x: any([n in x for n in tags]))]
+    return courses
 
 
-def parse_course(courses, node):
-    sat_courses = courses.copy()
+def parse_course(courses_input, node):
+    courses = courses_input.copy()
     for attrib in ["Disc", "Num"]:
-        sat_courses[attrib] = sat_courses[attrib].astype(str)
-        sat_courses = sat_courses[sat_courses[attrib].str.match(node.attrib[attrib].replace("@", ".+"))]
+        courses[attrib] = courses[attrib].astype(str)
+        courses = courses[courses[attrib].str.match(node.attrib[attrib].replace("@", ".+"))]
     for child_node in node:
         fun = switch({"With": parse_with}, child_node.tag)
-        sat_courses = fun(sat_courses, child_node) if fun else None
-    return sat_courses
+        courses = fun(courses, child_node) if fun else None
+    return courses
 
 
-def parse_exception(courses, node):
+def parse_except(courses_input, node):
+    courses = courses_input.copy()
     assert all([n.tag in ["Course"] for n in node])
-    sat_courses = courses.copy()
     for course in node.findall("Course"):
-        majors_match = sat_courses["Disc"] == course.attrib["Disc"]
-        nums_match = sat_courses["Num"] == course.attrib["Num"]
-        sat_courses = sat_courses[~(majors_match & nums_match)]
-    return sat_courses
+        majors_match = courses["Disc"] == course.attrib["Disc"]
+        nums_match = courses["Num"] == course.attrib["Num"]
+        courses = courses[~(majors_match & nums_match)]
+    return courses
 
 
-def parse_requirement(courses, node):
+def parse_requirement(node):
     tracked_attributes = ["Classes_begin", "Class_cred_op", "Connector", "Credits_end", "Credits_begin"]
     assert all([n.tag in ["Course", "Qualifier", "Except"] for n in node])
     assert all([n in tracked_attributes for n in node.attrib])
-    sat_courses_all = pd.DataFrame()
-    for course in node.findall("Course"):
-        sat_courses_all = pd.concat([sat_courses_all, parse_course(courses, course)])
-    for exception in node.findall("Exception"):
-        sat_courses_all = parse_course(sat_courses_all, exception)
-    satisfied = not sat_courses_all.empty
-    if "Classes_begin" in node.attrib and satisfied:
-        satisfied = len(sat_courses_all.index) >= int(node.attrib["Classes_begin"])
-    if "Credits_begin" in node.attrib and satisfied:
-        satisfied = pd.to_numeric(sat_courses_all["Credits"]).sum() >= int(node.attrib["Credits_begin"])
-    return satisfied
+
+    def requirement_function(courses):
+        sat_courses = pd.DataFrame()
+        possible_sat_courses = pd.DataFrame()
+        for course_node in node.findall("Course"):
+            sat_courses = pd.concat([sat_courses, parse_course(courses, course_node)])
+            possible_sat_courses = pd.concat([sat_courses, parse_course(courses_df, course_node)])
+        for exception_node in node.findall("Except"):
+            sat_courses = parse_course(sat_courses, exception_node)
+            possible_sat_courses = parse_course(courses_df, exception_node)
+        min_weight = possible_sat_courses["Credits"][possible_sat_courses["Credits"] != 0].min()
+        min_weight = min_weight if min_weight != 0 else 1
+        weight = int(not courses.empty) * min_weight
+        # if min_weight:
+        #     print(possible_sat_courses[["title", "Credits"]].sort_values(by="Credits"))
+        if bool(weight):
+            assert not all([n in node.attrib for n in ["Classes_begin", "Credits_begin"]])
+            if "Classes_begin" in node.attrib:
+                num_classes = int(node.attrib["Classes_begin"])
+                weight = int(len(courses.index) >= num_classes) * num_classes
+                weight = weight * min_weight if num_classes > 0 else 0
+            elif "Credits_begin" in node.attrib:
+                num_credits = int(int(node.attrib["Credits_begin"]))
+                weight = int(pd.to_numeric(courses["Credits"]).sum() >= num_credits) * num_credits
+                weight = weight if num_credits > 0 else 0
+        return weight
+
+    return [requirement_function]

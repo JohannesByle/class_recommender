ba829e42e2cddfa4cde85fc790dae63959886e06
sharondunbar <sharon.dunbar@my.wheaton.edu>
2021-04-03 15:45:24 -0500

    rating frontend works

--- flask_app/templates/my_classes/react_scripts/src/rate_class.jsx
+++ flask_app/templates/my_classes/react_scripts/src/rate_class.jsx
@@ -7,6 +7,23 @@ export function RateClassForm(course) {
 
     console.log(course)
 
+    let rating = null
+
+    function add_rating() {
+        if(rating == null)
+            return
+
+        course["rating"] = rating
+        fetch("/rate_class",
+            {
+                method: "POST",
+                body: JSON.stringify(course)
+            }
+        ).then(r => r.json()).then(
+            (result) => console.log(result),
+            (error) => console.log(error));
+    }
+
     ReactDOM.render(
         <div className="mx-auto" style={{maxWidth: 400}}>
             <div className="container mt-3 card bg-light border-secondary border-2"
@@ -16,15 +33,17 @@ export function RateClassForm(course) {
                         {course["title"]}
                     </div>
                     <div className="mb-2" id="grade_input">
-                        {ComboBox("Grade", ["1", "2", "3", "4", "5"], null)}
+                        {ComboBox("Rating", ["1", "2", "3", "4", "5"], (e, val) => rating = val)}
                     </div>
                 </div>
             </div>
-            {/*<button className="btn btn-secondary float-end mt-3" onClick={upload_class}>Add Class</button>*/}
+            <button className="btn btn-secondary float-end mt-3" onClick={add_rating}>Add Rating</button>
         </div>, document.getElementById("add_class_form")
 
     );
 }
+
+
 
 export default function render_rate_class_form() {
     render_classes(my_courses, false, true)
